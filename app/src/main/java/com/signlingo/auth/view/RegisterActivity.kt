40344ad487e81f3a797865b0fcf8e5f4003f43a7
Muhammad Ikhlas <mada.ikhlas@gmail.com>
2022-05-21 20:36:43 +0700

    Initial commit

---
+++ app/src/main/java/com/signlingo/auth/view/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.signlingo.auth.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.signlingo.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
---
+++ app/src/main/java/com/signlingo/main/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.signlingo.main.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.signlingo.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

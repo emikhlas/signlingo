8d6ddea1fb5d526ed8063650189d56c66d6c591a
Muhammad Ikhlas <mada.ikhlas@gmail.com>
2022-05-24 22:12:18 +0700

    Rough layout for home, missing quiz card due to difficult in implementing design

--- app/src/main/java/com/signlingo/main/view/MainActivity.kt
+++ app/src/main/java/com/signlingo/main/view/MainActivity.kt
@@ -2,11 +2,34 @@ package com.signlingo.main.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.signlingo.R
+import com.signlingo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tvSeeall.setOnClickListener{
+            if (binding.row3.visibility == View.GONE){
+                binding.row3.visibility = View.VISIBLE
+                binding.row4.visibility = View.VISIBLE
+                binding.row5.visibility = View.VISIBLE
+                binding.row6.visibility = View.VISIBLE
+                binding.tvSeeall.text = "See Less"
+            } else {
+                binding.row3.visibility = View.GONE
+                binding.row4.visibility = View.GONE
+                binding.row5.visibility = View.GONE
+                binding.row6.visibility = View.GONE
+                binding.tvSeeall.text = "See All"
+            }
+
+        }
     }
 }
